86652003ff2e5e987bd01695f397f3c855d9d17f
Amir <humarcrc@gmail.com>
2026-07-23 11:08:50 +0100

    Initialize package.json for Pi App Journey

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "pi-app-journey",
+  "version": "1.0.0",
+  "description": "Pi App Journey Backend",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "cors": "^2.8.5",
+    "dotenv": "^16.4.5",
+    "express": "^4.19.2"
+  }
+}
