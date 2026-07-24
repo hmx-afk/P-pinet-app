91bc07481cf8fb809bbb186efd09ea2062598437
Amir <humarcrc@gmail.com>
2026-07-24 22:59:27 +0100

    Add handler for completing payments

--- Complete.js
+++ Complete.js
@@ -1,1 +1,17 @@
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      error: "Method not allowed"
+    });
+  }
 
+  const { paymentId, txid } = req.body;
+
+  console.log("Complete Payment:", paymentId, txid);
+
+  return res.status(200).json({
+    success: true,
+    paymentId,
+    txid
+  });
+}
