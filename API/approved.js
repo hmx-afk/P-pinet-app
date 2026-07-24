4daf4e6afaebdbf1524fe915d96fdc491056fc6e
Amir <humarcrc@gmail.com>
2026-07-24 22:58:09 +0100

    Add POST handler for approving payments

---
+++ API/approved.js
@@ -0,0 +1,17 @@
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      error: "Method not allowed"
+    });
+  }
+
+  const { paymentId } = req.body;
+
+  console.log("Approve Payment:", paymentId);
+
+  return res.status(200).json({
+    success: true,
+    paymentId
+  });
+}
